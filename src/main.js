f79be302b1ab6ad15adeb6fcf850d6f6f76ed6a5
Anonymous <12345678+youruser@users.noreply.github.com>
2025-10-06 12:15:22 +0800

    Fine tune the show remaining behavior

--- src/main.js
+++ src/main.js
@@ -882,7 +882,7 @@ async function buildOverlayMain() {
       let label = document.createElement('span');
       label.style.fontSize = '12px';
 
-      if (sortByParts[0] === "remaining" || hideCompleted) {
+      if (sortByParts[0] === "remaining" || (hideCompleted && sortByParts[0] !== "painted")) {
         const remainingLabelText = (totalCount - paintedCount).toLocaleString();
         label.textContent = `${colorName} • ${remainingLabelText} Left`;
       } else {
