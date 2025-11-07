30513a781af439e3c291ed53d80603ce658ea057
Anonymous <12345678+youruser@users.noreply.github.com>
2025-11-08 07:13:36 +0800

    Switch to Dark UI for Dark Theme

--- src/main.js
+++ src/main.js
@@ -780,7 +780,7 @@ async function buildOverlayMain() {
             .addSelect({'id': 'bm-theme-setting'}, (instance, select) => {
               select.disabled = !templateManager.isThemeOverridden();
               const currentTheme = templateManager.getCurrentTheme();
-              Object.entries(themeList).forEach(([themeValue, displayText]) => {
+              Object.entries(themeList).forEach(([themeValue, [displayText, isDark]]) => {
                 const option = document.createElement('option');
                 option.value = themeValue;
                 option.textContent = displayText;
@@ -789,7 +789,7 @@ async function buildOverlayMain() {
               });
               select.addEventListener('change', async () => {
                 await templateManager.setCurrentTheme(select.value);
-                instance.handleDisplayStatus(`Changed the theme to "${themeList[select.value]}".`);
+                instance.handleDisplayStatus(`Changed the theme to "${themeList[select.value][0]}".`);
                 forceUpdateTheme();
               })
             }).buildElement()
