ea797a53692b5196f0d2e3f72880cf7d1bb14804
Anonymous <12345678+youruser@users.noreply.github.com>
2026-01-20 01:21:46 -0600

    Remove locked colors option if all unlocked

--- src/templateManager.js
+++ src/templateManager.js
@@ -1548,6 +1548,7 @@ export default class TemplateManager {
    * @since 0.85.17
    */
   isColorUnlocked(color) {
+    if (this.extraColorsBitmap === -1) return true; // all colors unlocked
     if (color < 32) return true;
     const mask = 1 << (color - 32);
     return (this.extraColorsBitmap & mask) !== 0;
