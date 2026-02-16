cd698ee32bfe1c101f8425838cb3913daac08ad5
Anonymous <12345678+youruser@users.noreply.github.com>
2026-02-16 01:09:20 +0100

    Change logo

--- src/RusMarble.meta.js
+++ src/RusMarble.meta.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Rus Marble
 // @namespace    https://github.com/SwingTheVine/
-// @version      0.87.12
+// @version      0.87.13
 // @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Rus Marble" icon is the Flag of Russia.
 // @author       korobka_konfet
 // @license      MPL-2.0
 // @supportURL   https://t.me/RusWplaceSupport_bot
 // @homepageURL  https://t.me/ruswplace
-// @icon         https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Russia.svg/960px-Flag_of_Russia.svg.png
+// @icon         https://raw.githubusercontent.com/korobkakonfet/rusmarble/custom-improve/dist/assets/logo_rusmarble.png
 // @match        https://wplace.live/*
 // @updateURL    https://github.com/korobkakonfet/rusmarble/raw/refs/heads/custom-improve/dist/RusMarble.user.js
 // @downloadURL  https://github.com/korobkakonfet/rusmarble/raw/refs/heads/custom-improve/dist/RusMarble.user.js
